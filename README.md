3205fcb9a9eb4acb392d9060672f29dc61864ede
huwngnosleep <thanhhung8210@gmail.com>
2021-04-23 09:45:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal_expense
+ this app is made to alert people for their daily expense
---
+++ android/app/src/main/kotlin/com/example/myfirstfltapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myfirstfltapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
